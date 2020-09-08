24a475e423b9ec8616108fc03e6a43e3bca4033d
Spencer Kenealy <kenealys@me.com>
2020-09-08 04:05:18 -0700

    User login route working with email or screenName

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { User } = require("../models/");
+const { Op } = require("sequelize");
 const { check, validationResult } = require("express-validator");
 const authMid = require("../middleware/authMid");
 const bcrypt = require("bcryptjs");
@@ -63,22 +64,65 @@ router.post(
 // api/users/login
 // login user
 // public
-// router.post("/login", [
-//   check("email")
-// ] async (req, res) => {
+router.post("/login", [
+  check("email", "Please include your email or screenName to login").not().isEmpty(),
+  check("password", "You must include your valid password to login").not().isEmpty()
+], async (req, res) => {
 
-//   const { screenName, email, password } = req.body;
+  let user_id;
+  const errors = validationResult(req);
 
-//   let user;
-//   user = await User.findOne({ where: { screenName } })
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-//   if (!user) {
-//     user = await User.findOne({ where: { email } });
-//   }
+  try {
+    const user = await User.findOne({
+      where: {
+        [Op.or]: [
+          { email: req.body.email },
+          { screenName: req.body.email }
+        ]
+      }
+    });
+    
 
-//   res.json(user);
+    if (user) {
+      user_id = user.id;
+    } else {
+      return res.status(400).json({ errors: [{ msg: "User with those credentials does not exist..." }] })
+    }
 
-// })
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+
+    if (!isMatch)  {
+      return res.status(400).json({ errors: [{ msg: "We could not verify your credentials. Please try again" }] })
+    }
+
+      // jwt
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        process.env.jwtSecret,
+        { expiresIn: "360000" },
+        (err, token) => {
+          if (err) {
+            console.error(err.message);
+          } else {
+            res.json({ token });
+          }
+        }
+      );
+
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
+})
 
 // get
 // api/users
