9e8d2093f0d91135d9faa41709df82bdd71cb411
Spencer Kenealy <kenealys@me.com>
2020-09-08 07:21:18 -0700

    User Routes Complete w/ frontend integration

--- client/src/components/layout/LoginForm.js
+++ client/src/components/layout/LoginForm.js
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { connect } from "react-redux";
 
-const LoginForm = () => {
+// redux
+import { login } from "../../actions/auth";
+
+const LoginForm = ({ login }) => {
+   const [userData, setUserData] = useState({
+      email: "",
+      password: ""
+   });
+
+   const { email, password } = userData;
+
+   const onChange = (e) => {
+      setUserData({ ...userData, [e.target.name]: e.target.value });
+   };
+
+   const onLogin = async (e) => {
+      e.preventDefault();
+      login({ email, password });
+   };
+
    return (
-      <form onSubmit={() => console.log("Login")}>
+      <form onSubmit={e => onLogin(e)}>
          <input
             type="text"
             name="email"
             placeholder="Email or Screen Name"
-            // onChange={(e) => onChange(e)}
+            onChange={(e) => onChange(e)}
          />
          <input
             autoComplete="new-password"
             type="password"
             name="password"
             placeholder="Password"
-            // onChange={(e) => onChange(e)}
+            onChange={(e) => onChange(e)}
          />
          <input id="register-btn" type="submit" value="Login" />
       </form>
    );
 };
 
-export default LoginForm;
+export default connect(null, { login })(LoginForm);
