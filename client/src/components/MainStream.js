15f7c7f8a6a5e1360d07919cfd545dec7f625687
SpencerKK <kenealys@me.com>
2020-09-22 13:52:00 -0700

    Added auto-height-expansion styling to mainstream textarea

--- client/src/components/MainStream.js
+++ client/src/components/MainStream.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, {useRef} from "react";
 
 const MainStream = () => {
+    const textAreaRef = useRef(null);
+    let heightLimit = 500;
+
+    const onAreaType = () => {
+        textAreaRef.current.style.height = "";
+        textAreaRef.current.style.height = Math.min(textAreaRef.current.scrollHeight, heightLimit) + "px";
+    }
+
    return (
       <div className="main-stream-wrapper">
          <div className="main-stream-header">
@@ -8,9 +16,14 @@ const MainStream = () => {
             <form>
                <div className="home-textarea-wrapper">
                   <div className="icon-holder">
-                  <i className="fas fa-user"></i>
+                     <i className="fas fa-user"></i>
                   </div>
-                  <textarea placeholder="What's Happening..."></textarea>
+                  <textarea
+                    ref={textAreaRef}
+                    // style={{ backgroundColor: "pink" }}
+                    onChange={onAreaType}
+                     placeholder="What's Happening..."
+                  ></textarea>
                </div>
                <div className="post-addl-wrapper">
                   <div className="add-btns">
