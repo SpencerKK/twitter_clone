3288c78717411375c0ddb7b2115026fbdaf2d548
Spencer Kenealy <kenealys@me.com>
2020-09-28 14:03:45 -0700

    associations for followers included. Follow routes next

--- models/index.js
+++ models/index.js
@@ -5,4 +5,21 @@ User.hasMany(Post);
 
 Post.belongsTo(User);
 
+// user's followers
+User.belongsToMany(User, {
+    as: "followed_user",
+    through: "followers",
+    foreignKey: "followed_user_id",
+    otherKey: "follower_id"
+});
+
+// users a user follows
+User.belongsTo(User, {
+    as: "follower",
+    through: "followers",
+    foreignKey: "follower_id",
+    otherKey: "followed_user_id"
+})
+
+
 module.exports = { User, Post };
