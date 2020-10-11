03c2b2bfb9b8ed3b4afc2e8ac79044ce9c56d351
Spencer Kenealy <kenealys@me.com>
2020-10-11 16:53:34 -0700

    styled user cards on the connect component

--- client/src/components/subcomponents/Connect.js
+++ client/src/components/subcomponents/Connect.js
@@ -3,30 +3,34 @@ import { connect } from "react-redux";
 import { renderActiveUsers } from "../../actions/Needs2Follow/homeSubs";
 
 const Connect = ({ renderActiveUsers, activeUsers }) => {
-
-    useEffect(() => {
-        renderActiveUsers();
-    }, []);
+   useEffect(() => {
+      renderActiveUsers();
+   }, []);
 
    return (
       <div className="connect">
-          {
-              activeUsers && activeUsers.users.map(user => (
-              <div className="connect-user-card">
+         {activeUsers &&
+            activeUsers.users.map((user) => (
+               <div className="connect-user-card">
                   <i className="fas fa-user"></i>
                   <div className="connect-user-info">
-                  <p>{user.screenName}</p>
-                  <p>Proident occaecat ex cillum aute sunt excepteur aute dolor ad est consequat magna.</p>
+                     <div className="connect-head">
+                        <p id="screenName">{user.screenName}</p>
+                        <button>Follow</button>
+                     </div>
+                     <p id="detail">
+                        Proident occaecat ex cillum aute sunt excepteur aute
+                        dolor ad est consequat magna.
+                     </p>
                   </div>
-              </div>
-              ))
-          }
+               </div>
+            ))}
       </div>
    );
 };
 
-const mapStateToProps = state => ({
-    activeUsers: state.activeUsers.activeUsers
-})
+const mapStateToProps = (state) => ({
+   activeUsers: state.activeUsers.activeUsers,
+});
 
 export default connect(mapStateToProps, { renderActiveUsers })(Connect);
