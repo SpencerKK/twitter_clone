f730453d4c4a62ee8909508d1757e8f7e195822c
Spencer Kenealy <kenealys@me.com>
2020-10-18 17:09:33 -0700

    Rightbar now lists 'users to follow' each with its own follow button
    Once a user clicks on the follow button, the option to follow that specific user disappears and their posts are shown in the main stream

--- client/src/components/RightBar.js
+++ client/src/components/RightBar.js
@@ -1,11 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
 
-const RightBar = () => {
-    return (
-        <div className="right-bar">
-            <p>Who To Follow</p>
-        </div>
-    )
-}
+// actions
+import { renderActiveUsers } from "../actions/Needs2Follow/homeSubs";
+import { followUser } from "../actions/follow";
+import { getFollowingPosts } from "../actions/followingPosts";
 
-export default RightBar;
+const RightBar = ({ activeUsers, renderActiveUsers, getFollowingPosts, followUser }) => {
+    let [loading, setLoading] = useState(true);
+
+   useEffect(() => {
+      renderActiveUsers();
+      getFollowingPosts();
+   }, [loading]);
+
+   const onFollow = (e, followed_id) => {
+       e.preventDefault();
+       followUser(followed_id);
+       setLoading(!loading);
+       getFollowingPosts();
+       renderActiveUsers();
+   }
+
+   console.log(loading);
+
+   return (
+      <div className="right-bar">
+         <div className="user-list">
+            <p id="who-to-follow">Who to Follow</p>
+            {activeUsers &&
+               activeUsers.potentialFollowUsers.map((user) => (
+                  <div className="user-card">
+                     <div className="user-card-text">
+                        <i className="fas fa-user"></i>
+                        <p>{user.screenName}</p>
+                     </div>
+                     <button onClick={e => onFollow(e, { followed_id: user.id })}>Follow</button>
+                  </div>
+               ))}
+         </div>
+      </div>
+   );
+};
+
+const mapStateToProps = (state) => ({
+   activeUsers: state.activeUsers.activeUsers,
+});
+
+export default connect(mapStateToProps, { renderActiveUsers, followUser, getFollowingPosts })(RightBar);
