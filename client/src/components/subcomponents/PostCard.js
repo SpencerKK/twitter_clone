1e272178d54e9d4b8466a46161a39c045e752b91
Spencer Kenealy <kenealys@me.com>
2020-10-28 10:27:38 -0700

    got rid of loading state in postcard

--- client/src/components/subcomponents/PostCard.js
+++ client/src/components/subcomponents/PostCard.js
@@ -5,57 +5,63 @@ import { connect } from "react-redux";
 import { likePost } from "../../actions/likes";
 import { getFollowingPosts } from "../../actions/followingPosts";
 
-const PostCard = ({ screenName, postContent, postId, isLiked, likeCount, getFollowingPosts, followingPosts, likePost }) => {
-   const [loaded, setLoaded] = useState(false);
-
+const PostCard = ({
+   screenName,
+   postContent,
+   postId,
+   isLiked,
+   likeCount,
+   getFollowingPosts,
+   followingPosts,
+   likePost,
+}) => {
    const onLikePost = (postId) => {
       likePost(postId);
       getFollowingPosts();
    };
 
-   useEffect(() => {
-      setLoaded(true)
-   }, [])
-
-
    return (
       <div className="post-card">
-      <div className="post-body">
-         <div className="post-user-icon">
-            <i className="fas fa-user"></i>
-         </div>
-         <div className="post-content">
-            <div className="post-card-username">
-               <p>{screenName}</p>
+         <div className="post-body">
+            <div className="post-user-icon">
+               <i className="fas fa-user"></i>
             </div>
-            <div className="post-text">
-               <p>{postContent}</p>
+            <div className="post-content">
+               <div className="post-card-username">
+                  <p>{screenName}</p>
+               </div>
+               <div className="post-text">
+                  <p>{postContent}</p>
+               </div>
+            </div>
+         </div>
+         <div className="post-respose-opt">
+            <div className="post-option-wrapper">
+               <i className="far fa-comment"></i>
+               <p>450</p>
+            </div>
+            <div className="post-option-wrapper">
+               <i
+                  className={
+                     isLiked && isLiked === true
+                        ? "fas fa-heart"
+                        : "far fa-heart"
+                  }
+                  // className="far fa-heart"
+                  style={isLiked && isLiked === true ? { color: "red" } : null}
+                  onClick={() => onLikePost(postId)}
+               ></i>
+               <p>{likeCount}</p>
             </div>
          </div>
       </div>
-      <div className="post-respose-opt">
-         <div className="post-option-wrapper">
-            <i className="far fa-comment"></i>
-            <p>450</p>
-         </div>
-         <div className="post-option-wrapper">
-            <i
-               className={isLiked ? "fas fa-heart" : "far fa-heart"}
-               // className="far fa-heart"
-               style={isLiked && isLiked === true ? { color: "red" } : null}
-               onClick={() => onLikePost(postId)}
-            ></i>
-            <p>{likeCount}</p>
-         </div>
-      </div>
-   </div>
    );
-
-
 };
 
-const mapStateToProps = state => ({
-    followingPosts: state.followingPosts.isFollowingPosts,
-})
+const mapStateToProps = (state) => ({
+   followingPosts: state.followingPosts.isFollowingPosts,
+});
 
-export default connect(mapStateToProps, { likePost, getFollowingPosts })(PostCard);
+export default connect(mapStateToProps, { likePost, getFollowingPosts })(
+   PostCard
+);
