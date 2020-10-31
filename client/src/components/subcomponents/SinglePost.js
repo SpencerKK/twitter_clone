c58a46d034bb985623c37069e53dc4a9b8a873e5
Spencer Kenealy <kenealys@me.com>
2020-10-30 18:14:57 -0700

    singlePost.js card is now displaying post screenName and content

--- client/src/components/subcomponents/SinglePost.js
+++ client/src/components/subcomponents/SinglePost.js
@@ -1,33 +1,42 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const SinglePost = () => {
+const SinglePost = ({ singlePost }) => {
    return (
       <div className="single-post">
-         <div className="post-header">
-            <div className="post-user-icon">
-               <i className="fas fa-user"></i>
-            </div>
-            <p id="screenName">screenName</p>
-         </div>
-         <div className="post-body">
-             Ex cillum duis commodo excepteur sint exercitation commodo sint eiusmod mollit anim. Ex cillum duis commodo excepteur sint exercitation commodo sint eiusmod mollit anim.
-         </div>
-         <div className="post-data">
-            <div id="post-likes">
-               <p className="count">1.8k</p>
-               <p>Likes</p>
-            </div>
-            <div id="post-comments">
-               <p className="count">103</p>
-               <p>Comments</p>
-            </div>
-         </div>
-         <div className="post-actions">
-            <i className="far fa-heart"></i>
-            <i className="far fa-comment"></i>
-         </div>
+         {singlePost && singlePost !== null ? (
+            <>
+               <div className="post-header">
+                  <div className="post-user-icon">
+                     <i className="fas fa-user"></i>
+                  </div>
+                  <p id="screenName">{singlePost.screenName}</p>
+               </div>
+               <div className="post-body">
+                  <p>{singlePost.content}</p>
+               </div>
+               <div className="post-data">
+                  <div id="post-likes">
+                     <p className="count">1.8k</p>
+                     <p>Likes</p>
+                  </div>
+                  <div id="post-comments">
+                     <p className="count">103</p>
+                     <p>Comments</p>
+                  </div>
+               </div>
+               <div className="post-actions">
+                  <i className="far fa-heart"></i>
+                  <i className="far fa-comment"></i>
+               </div>
+            </>
+         ) : <p>loading...</p>}
       </div>
    );
 };
 
-export default SinglePost;
+const mapStateToProps = (state) => ({
+   singlePost: state.singlePost.singlePost,
+});
+
+export default connect(mapStateToProps, {})(SinglePost);
