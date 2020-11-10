1145e790f98e1b50b1d5d836d81e1eac6d201cab
SpencerKK <kenealys@me.com>
2020-11-10 10:13:21 -0800

    added code to onLikePost in SinglePost.js that correctly updates the like count on the page
    I think there might be something I can still fix, but 9.9 times out of 10, the like count is properly displayed on the screen

--- client/src/components/subcomponents/SinglePost.js
+++ client/src/components/subcomponents/SinglePost.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { connect } from "react-redux";
 import CommentCard from "./CommentCard";
 
@@ -8,155 +8,157 @@ import { getPostComments } from "../../actions/singlePost";
 import { getSinglePost } from "../../actions/singlePost";
 import { renderSinglePost } from "../../actions/singlePostSubs";
 import { likePost } from "../../actions/likes";
+import { getFollowingPosts } from "../../actions/followingPosts";
 
 const SinglePost = ({
-   singlePost,
-   postLikeCount,
-   postCommentCount,
-   singlePostComments,
-   postComment,
-   likePost,
-   getPostComments,
-   getSinglePost,
-   renderSinglePost,
+  singlePost,
+  postLikeCount,
+  postCommentCount,
+  singlePostComments,
+  getFollowingPosts,
+  postComment,
+  likePost,
+  getPostComments,
+  getSinglePost,
+  renderSinglePost,
 }) => {
-   const [postContent, setPostContent] = useState(null);
-   const textAreaRef = useRef(null);
-   let heightLimit = 500;
+  const [postContent, setPostContent] = useState(null);
+  const textAreaRef = useRef(null);
+  let heightLimit = 500;
 
-   const onAreaType = (e) => {
-      textAreaRef.current.style.height = "";
-      textAreaRef.current.style.height =
-         Math.min(textAreaRef.current.scrollHeight, heightLimit) + "px";
+  const onAreaType = (e) => {
+    textAreaRef.current.style.height = "";
+    textAreaRef.current.style.height =
+      Math.min(textAreaRef.current.scrollHeight, heightLimit) + "px";
 
-      setPostContent(e.target.value);
-   };
+    setPostContent(e.target.value);
+  };
 
-   // TextArea height ^ Modal stuff v
+  // TextArea height ^ Modal stuff v
 
-   let [modalStatus, setModalStatus] = useState("none");
+  let [modalStatus, setModalStatus] = useState("none");
 
-   const openModal = () => {
-      setModalStatus("block");
-   };
+  const openModal = () => {
+    setModalStatus("block");
+  };
 
-   const closeModal = () => {
-      setModalStatus("none");
-   };
+  const closeModal = () => {
+    setModalStatus("none");
+  };
 
-   const onLikePost = ({ singlePost }) => {
-      let postId = singlePost.id;
-      likePost(postId);
+  const onLikePost = ({ singlePost }) => {
+    let postId = singlePost.id;
+    likePost(postId);
+    getFollowingPosts().then(() => {
       getSinglePost(postId);
-      // right now, the updating of the like count on singlePost is acting janky. Will update soon
-   }
+    });
+  };
 
-   const onCommentSubmit = ({ singlePost }) => {
-      let content = postContent;
-      let postId = singlePost.id;
-      postComment({ content, postId });
-      getSinglePost(postId);
-      closeModal();
-   };
+  const onCommentSubmit = ({ singlePost }) => {
+    let content = postContent;
+    let postId = singlePost.id;
+    postComment({ content, postId });
+    getSinglePost(postId);
+    closeModal();
+  };
 
-   return (
-      <div className="single-post">
-         {singlePost !== null && singlePostComments !== null ? (
-            <>
-               <div className="post-header">
-                  <div className="post-user-icon">
-                     <i className="fas fa-user"></i>
-                  </div>
-                  <p id="screenName">{singlePost.screenName}</p>
-               </div>
-               <div className="post-body">
-                  <p>{singlePost.content}</p>
-               </div>
-               <div className="post-data">
-                  <div id="post-likes">
-                     <p className="count">{postLikeCount}</p>
-                     <p>Likes</p>
-                  </div>
-                  <div id="post-comments">
-                     <p className="count">{postCommentCount}</p>
-                     <p>Comments</p>
-                  </div>
-               </div>
-               <div className="post-actions">
-                  <i className="far fa-heart" onClick={() => onLikePost({singlePost})}></i>
-                  <i className="far fa-comment" onClick={() => openModal()}></i>
-               </div>
-               <div
-                  className="comment-modal-wrapper"
-                  style={{ display: modalStatus }}
-               >
-                  <div className="comment-modal">
-                     <i
-                        className="fas fa-times"
-                        onClick={() => closeModal()}
-                     ></i>
-                     <div className="post-header">
-                        <div className="post-user-icon">
-                           <i className="fas fa-user"></i>
-                        </div>
-                        <p id="screenName">{singlePost.screenName}</p>
-                     </div>
-                     <div className="post-body">
-                        <p>{singlePost.content}</p>
-                     </div>
-                     <div className="comment-area">
-                        <div className="post-user-icon">
-                           <i className="fas fa-user"></i>
-                        </div>
-                        <div className="comment-textarea-wrapper">
-                           <textarea
-                              ref={textAreaRef}
-                              placeholder="Post Your Reply"
-                              onChange={(e) => onAreaType(e)}
-                           ></textarea>
-                        </div>
-                     </div>
-                     <div className="modal-submit-wrapper">
-                        <button
-                           onClick={() => onCommentSubmit({ singlePost })}
-                        >
-                           Reply
-                        </button>
-                     </div>
-                  </div>
-               </div>
-               {singlePostComments.length <= 0 ? (
-                  <p id="no-comments">Be The First Comment!</p>
-               ) : (
-                  singlePostComments.map((comment) => (
-                     <CommentCard
-                        screenName={comment.screenName}
-                        content={comment.content}
-                     />
-                  ))
-               )}
-               {singlePostComments.length <= 0 ? null : (
-                   <p id="end-of-comments">End of Comments</p>
-               )}
-            </>
-         ) : (
-            <p>loading...</p>
-         )}
-      </div>
-   );
+  return (
+    <div className="single-post">
+      {singlePost !== null && singlePostComments !== null ? (
+        <>
+          <div className="post-header">
+            <div className="post-user-icon">
+              <i className="fas fa-user"></i>
+            </div>
+            <p id="screenName">{singlePost.screenName}</p>
+          </div>
+          <div className="post-body">
+            <p>{singlePost.content}</p>
+          </div>
+          <div className="post-data">
+            <div id="post-comments">
+              <p className="count">{postCommentCount}</p>
+              <p>Comments</p>
+            </div>
+            <div id="post-likes">
+              <p className="count">{postLikeCount}</p>
+              <p>Likes</p>
+            </div>
+          </div>
+          <div className="post-actions">
+            <i className="far fa-comment" onClick={() => openModal()}></i>
+            <i
+              className="far fa-heart"
+              onClick={() => onLikePost({ singlePost })}
+            ></i>
+          </div>
+          <div
+            className="comment-modal-wrapper"
+            style={{ display: modalStatus }}
+          >
+            <div className="comment-modal">
+              <i className="fas fa-times" onClick={() => closeModal()}></i>
+              <div className="post-header">
+                <div className="post-user-icon">
+                  <i className="fas fa-user"></i>
+                </div>
+                <p id="screenName">{singlePost.screenName}</p>
+              </div>
+              <div className="post-body">
+                <p>{singlePost.content}</p>
+              </div>
+              <div className="comment-area">
+                <div className="post-user-icon">
+                  <i className="fas fa-user"></i>
+                </div>
+                <div className="comment-textarea-wrapper">
+                  <textarea
+                    ref={textAreaRef}
+                    placeholder="Post Your Reply"
+                    onChange={(e) => onAreaType(e)}
+                  ></textarea>
+                </div>
+              </div>
+              <div className="modal-submit-wrapper">
+                <button onClick={() => onCommentSubmit({ singlePost })}>
+                  Reply
+                </button>
+              </div>
+            </div>
+          </div>
+          {singlePostComments.length <= 0 ? (
+            <p id="no-comments">Be The First Comment!</p>
+          ) : (
+            singlePostComments.map((comment) => (
+              <CommentCard
+                screenName={comment.screenName}
+                content={comment.content}
+              />
+            ))
+          )}
+          {singlePostComments.length <= 0 ? null : (
+            <p id="end-of-comments">End of Comments</p>
+          )}
+        </>
+      ) : (
+        <p>loading...</p>
+      )}
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => ({
-   singlePost: state.singlePost.singlePost,
-   postLikeCount: state.singlePost.postLikeCount,
-   postCommentCount: state.singlePost.postCommentCount,
-   singlePostComments: state.singlePostComments.postComments,
+  singlePost: state.singlePost.singlePost,
+  postLikeCount: state.singlePost.postLikeCount,
+  postCommentCount: state.singlePost.postCommentCount,
+  singlePostComments: state.singlePostComments.postComments,
 });
 
 export default connect(mapStateToProps, {
-   postComment,
-   getPostComments,
-   getSinglePost,
-   renderSinglePost,
-   likePost
+  postComment,
+  getPostComments,
+  getFollowingPosts,
+  getSinglePost,
+  renderSinglePost,
+  likePost,
 })(SinglePost);
